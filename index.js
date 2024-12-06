ee804077e6d0d5f6262b490b3408f8bc47ba2442
Elijah <iamngunjiri@gmail.com>
2024-12-06 13:25:02 +0300

    Complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Elijah",
+    streetAddress: "Nairobi",
+}
+
+const updateEmployeeWithKeyAndValue = (obj, key, value) => ({...obj, [key]: value})
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (obj, key, value) => {
+    obj[key] = value
+    return obj
+}
+
+const deleteFromEmployeeByKey = (obj, key) => {
+    const newObj = {...obj}
+    delete newObj[key]
+    return newObj
+}
+
+const destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+    delete obj[key]
+    return obj
+} 
